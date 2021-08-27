ea39923d11021bcde1a82498d957cdf3c77e70b6
DamianoDF <papricar@libero.it>
2021-08-27 17:52:48 +0200

    secondo commit

---
+++ src/test/java/it/failutti/damiano/RadioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.failutti.damiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
